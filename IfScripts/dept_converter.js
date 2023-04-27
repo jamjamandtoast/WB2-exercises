123a0f30ea63b5cd12736f7a056c24c8f21c7aa0
Jams Jams <yarinyroblero@Jamyss-iMac.local>
2023-04-27 14:49:27 -0400

    finished

---
+++ IfScripts/dept_converter.js
@@ -0,0 +1,54 @@
+"Use Strict";
+
+let departmentCode = 20;
+let departmentName;
+
+
+switch (departmentCode) {
+    case 1:
+        departmentName = "Marketing";
+        break;
+    case 5:
+        departmentName = "Human Resources";
+        break;
+    case 10:
+    departmentName = "Accounting";
+    break; 
+    case 12:
+        departmentName = "Legal";
+        break;
+    case 18:
+        departmentName = "IT";
+        break;
+    case 20:
+        departmentName = "Customer Relations"  
+}
+
+console.log("The name of this department is " + departmentName + " and code is " + departmentCode);
+console.log("The name of this department is" + Marketing + " and code is " + 1) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
