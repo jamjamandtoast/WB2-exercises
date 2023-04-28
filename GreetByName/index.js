3bbdc5d147af71794003cc2e2dd89a11b228a7a8
Jennifer Roblero <jenniferroblero@Jennifers-MacBook-Pro.local>
2023-04-28 14:59:32 -0400

    finished checked work

--- GreetByName/index.js
+++ GreetByName/index.js
@@ -1,10 +1,23 @@
-"use strict"
+"use strict";
 
 window.onload = init;
+
 function init() {
-const greetbtn = document.getElementById("nameField");
-greetbtn.onclick = onGreetUserBtnClicked
+    const greetbtn = document.getElementById("greetBtn");
+    greetbtn.onclick = onGreetUserBtnClicked
 }
+
 function onGreetUserBtnClicked() {
-    alert("Hello UserName Here")
+    // get the initial known values (this time from the textboxes on your html)
+    let username = nameField.value;
+
+
+
+    //compute the unknown values
+    let message = `Hello ${username}!`;
+
+
+
+    ///display the result.
+    alert(message)
 }
